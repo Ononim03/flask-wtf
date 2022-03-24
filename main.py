d45f97d100ae5cfb5efeb0294908259a701078e2
Ononim <tzinatullin16@gmail.com>
2022-03-24 17:37:05 +0300

    3 classwork

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+               'климатолог', 'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+               'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман',
+               'пилот дронов']
 
 
 @app.route('/<title>')
@@ -15,6 +19,11 @@ def index(title):
 def training(prof):
     profession = 'и' if 'инженер' in prof or 'строитель' in prof else 'н'
     return render_template('training.html', prof=profession)
+
+
+@app.route('/list_prof/<type>')
+def list_prof(type):
+    return render_template('list.html', type=type, list=professions)
 
 
 if __name__ == '__main__':
